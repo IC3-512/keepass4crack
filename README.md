b5c4f4fb95310a485d226bfc45ca398a2200ee3b
Max Entleutner <max.entleutner@gmail.com>
2025-10-30 00:20:48 +0100

    init

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # keepass4crack
 python keepass4 cracker
+
+``` 
+usage: main.py [-h] -d DATABASE -w WORDLIST [-k KEYFILE] [-t THREADS]
+
+simple multi-process brute-force tool for KeePass (.kdbx) files. Works with password-only or password+keyfile
+setups.
+
+options:
+  -h, --help            show this help message and exit
+  -d, --database DATABASE
+                        Path to the KeePass .kdbx file
+  -w, --wordlist WORDLIST
+                        Text file with passwords to try, one per line
+  -k, --keyfile KEYFILE
+                        Optional keyfile to use if the database requires it
+  -t, --threads THREADS
+                        Number of parallel processes to use (default: all CPU cores)
+
+``` 
+
+
+```
+uv run main.py -d target.kdbx -w /usr/share/wordlists/rockyou.txt -t 20
+Database file : target.kdbx
+Wordlist      : /usr/share/wordlists/rockyou.txt
+Threads used  : 20
+
+Loaded 14344306 passwords to test
+Testing passwords :D:   0%|                                            | 45/14344306 [00:04<393:40:45 , 9.4 pwd/sec]
+``` 
+
+
+
--- main.py
+++ main.py
@@ -21,24 +21,19 @@ def main():
     parser.add_argument("-d", "--database", type=ascii, required=True, help="Path to the KeePass .kdbx file")
     parser.add_argument("-w", "--wordlist", type=ascii, required=True, help="Text file with passwords to try, one per line")
     parser.add_argument("-k", "--keyfile", type=ascii, required=False, help="Optional keyfile to use if the database requires it")
-    parser.add_argument("-o", "--output", action="store_true", help="If the database is unlocked, show all stored entries")
-    parser.add_argument("-f", "--outfile", type=str, help="Save dumped entries to a text file")
     parser.add_argument("-t", "--threads", type=int, default=os.cpu_count(), help="Number of parallel processes to use (default: all CPU cores)")
     args = parser.parse_args()
 
     db_file = args.database.replace("'", "")
     wordlist_file = args.wordlist.replace("'", "")
     keyfile_path = args.keyfile.replace("'", "") if args.keyfile else None
-    output_entries = args.output
-    output_file = args.outfile
     num_threads = args.threads
 
     print(f"Database file : {db_file}")
     print(f"Wordlist      : {wordlist_file}")
     if keyfile_path:
         print(f"Keyfile       : {keyfile_path}")
-    if output_file:
-        print(f"Output file   : {output_file}")
+
 
     max_threads = os.cpu_count()
     if num_threads < 1:
